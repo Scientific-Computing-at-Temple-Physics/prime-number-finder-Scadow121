019431a21de08c127733354c4ab44747f39b62ad
Alex <Alex@MacBook-Pro-6.local>
2018-02-01 19:05:41 -0500

    Committing Prime Number Finder Project

--- primes.py
+++ primes.py
@@ -1,6 +1,7 @@
 # This is a comment.  Python will ignore these lines (starting with #) when running
 
 import math as ma
+import time
 # To use a math function, write "ma." in front of it.  Example:  ma.sin(3.146)
 
 # These functions will ask you for your number ranges, and assign them to 'x1' and 'x2'
@@ -16,3 +17,33 @@ If no prime numbers are found in that range, it should print a statement to the 
 Now we end the comment with triple quotes."""
 
 # The rest is up to you!
+
+x1=int(x1)
+x2=int(x2)
+if x1 < x2:
+    print "The prime numbers between" ,x1, "and" ,x2, "are:"
+    x = x1
+    d = 2
+    while x != (x2+1):
+        if x != d:
+            q = x%d
+            if q != 0:
+                if d >= x:
+                    if x > 0:
+                        print x
+                        x = x+1
+                        d = 2
+                    else:
+                        x = x+1
+                        d = 2
+                else:
+                    d = d+1
+            else:
+                x = x+1
+                d = 2
+        else:d = d+1
+else:
+    print "Invalid Input Detected!"
+    time.sleep(0.5)
+    print "Initiating Self-Destruct Sequence (This may take a while)..."
+    time.sleep(10)
